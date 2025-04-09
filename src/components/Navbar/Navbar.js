1c5569a125553c4945a1a33815d1aa078f70a3f1
alisherova <alisherovanurjahon@gmail.com>
2025-04-09 17:23:51 +0500

    create new project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SearchButton = styled.button`
+    background-color: green;
+    color: white;
+    padding: 4px 12px;
+    border-radius: 12px;
+    border: none;
+`
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SearchButton } from './Navbar'
+
+const Navbar = () => {
+  return (
+    <SearchButton>
+        Search
+    </SearchButton>
+  )
+}
+
+export default Navbar
